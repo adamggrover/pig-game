602d444b5d4b1e616b262b711b7b7a1b037cb27c
adamggrover <93373074+adamggrover@users.noreply.github.com>
2022-11-07 19:12:34 +0000

    Add pseudo code to begin solving problem

--- pig-game/Program.cs
+++ pig-game/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// initialise variables: dice roll, round score, total score, winning score
+
+// player 1 enter your name
+
+// assign player 1 user input to variable
+
+// player 2 enter your name
+
+// assign player 2 user input to variable
+
+// roll dice
+
+// tell player 1 result of dice roll, round total, running total
+
+// ask player 1 if they would like to roll again or hold
+
+// repeat process for player 2
+
+// if winning score is reached
